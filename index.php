35e01a8c3903251948f01a44654b9938466bef7a
humunuk <siimkallari@gmail.com>
2016-02-28 18:27:30 +0200

    Moved some attributes to CSS as recommended by W3C validator

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
-<table border="1" width="500" height="500">
+<table>
     <caption><h3>CSS, HTML, PHP, MySQL</h3></caption>
     <thead>
     <tr>
@@ -37,5 +37,12 @@
     </tr>
     </tbody>
 </table>
+
+
+<p>
+    <a href="http://validator.w3.org/check?uri=referer">
+        <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+    </a>
+</p>
 </body>
 </html>
