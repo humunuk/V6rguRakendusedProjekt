82fd32af8d5054b2efe0dc9ef5ce491bb5a7753f
humunuk <siimkallari@gmail.com>
2016-02-28 18:20:31 +0200

    First part of second exercise (Create table and modify it)

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Võrgurakendused kodused ülesanded</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<table border="1" width="500" height="500">
+    <caption><h3>CSS, HTML, PHP, MySQL</h3></caption>
+    <thead>
+    <tr>
+        <th>CSS</th>
+        <th>HTML</th>
+        <th>PHP</th>
+        <th>MySQL</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+       <td colspan="3">
+       </td>
+        <td rowspan="3"></td>
+    </tr>
+    <tr>
+        <td rowspan="3">
+        </td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td colspan="3"></td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
